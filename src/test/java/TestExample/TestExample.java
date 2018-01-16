1e58ae2419b7f1bd5d3c2cc24a1ac010b4c0c11f
apzz0928 <apzz0928@gmail.com>
2018-01-16 10:28:13 +0900

    2018/01/16 : blogspot, egloos 테스트용

---
+++ src/test/java/TestExample/TestExample.java
@@ -0,0 +1,181 @@
+package TestExample;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
+
+import com.codeborne.selenide.testng.ScreenShooter;
+
+public class TestExample {      
+ 	   private static WebDriver driver;
+ 	   private static JavascriptExecutor js;
+ 	   private static String blogspotUrl, egloosUrl, hubUrl;
+       private static String TestBrowser;
+       
+       @Parameters("browser")
+       @BeforeClass
+       public void beforeTest(String browser) throws MalformedURLException {
+ 	       blogspotUrl = "http://apzz0928.blogspot.com/";
+ 	       egloosUrl = "http://apzz0928.egloos.com/";
+    	   hubUrl = "http://10.77.249.69:5555/wd/hub";
+ 	       
+    	   String urlToRemoteWD = hubUrl;
+    	   DesiredCapabilities cap;
+    	   ScreenShooter.captureSuccessfulTests = false;
+    	   
+    	   if(browser.equals("chrome")){
+    		   TestBrowser = "chrome";
+    		   cap = DesiredCapabilities.chrome();
+    		   RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
+    		   WebDriverRunner.setWebDriver(driver);
+     		} else if(browser.equals("firefox")) {
+     			TestBrowser = "firefox";
+     			cap = DesiredCapabilities.firefox();
+     			RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
+     			WebDriverRunner.setWebDriver(driver);
+     		} else if(browser.equals("edge")) {
+     			TestBrowser = "edge";
+     			cap = DesiredCapabilities.edge();
+     			RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
+     			WebDriverRunner.setWebDriver(driver);
+     		} else if(browser.equals("internetExplorer")) {
+     			TestBrowser = "internetExplorer";
+     			cap = DesiredCapabilities.internetExplorer();
+     			cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true); //IE 실행을 위한 보안 관련 설정 임시 변경
+     			cap.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false); //ie text 입력 속도 향상을 위한 부분
+     			cap.setCapability("requireWindowFocus", true); //ie text 입력 속도 향상을 위한 부분
+     			cap.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true); //ie 캐시 삭제를 위한 부분
+     			RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
+     			WebDriverRunner.setWebDriver(driver);
+     		} driver.manage().window().setSize(new Dimension(1400, 900));
+       }
+ 	   
+       @Test(priority = 0)
+       public void blogspotTest() {
+ 	       open(blogspotUrl);
+ 	       //외부배너 클릭
+ 	       $(".banner1").click();
+ 	       $(By.linkText("자세한 내용 보기")).click();
+ 	       $(".img1").click();
+ 	       $("#uid").waitUntil(appear, 5000);
+ 	       open(blogspotUrl);
+ 	       $(".banner1").click();
+ 	       $(By.linkText("자세한 내용 보기")).click();
+ 	       $(".img2").click();
+ 	       $(".banner1").waitUntil(appear, 5000);
+ 	       open(blogspotUrl);
+ 	       
+ 	       //내부배너 클릭
+	       int x = 1;
+    	   for(int i = 0;i<=3;i++) {
+    		   $(".image-attribution").waitUntil(text("테마 이미지 제공:"), 3000);
+ 	 	       $(".innerBanner" + x ).click();
+ 	 	       System.out.println("Banner i is : " + i + " / x is : " + x );
+ 	 	       x++;
+ 	 	   }System.out.println(" -- Banner click end -- ");
+ 	       //마케팅 클릭
+	       x = 1;
+    	   for(int i = 0;i<=4;i++) {
+    		   $(".image-attribution").waitUntil(text("테마 이미지 제공:"), 3000);
+ 	 	       $(".sub" + x).click();
+ 	 	       System.out.println("marketing i is : " + i + " / x is : " + x );
+ 	 	       x++;
+ 	 	   }System.out.println(" -- marketing click end -- ");
+ 	       //인하우스 클릭
+	       x = 6;
+    	   for(int i = 0;i<=2;i++) {
+    		   $(".image-attribution").waitUntil(text("테마 이미지 제공:"), 3000);
+ 	 	       $(".sub" + x).click();
+ 	 	       System.out.println("inHouse i is : " + i + " / x is : " + x );
+ 	 	       x++;
+ 	 	   }System.out.println(" -- inHouse click end -- ");
+ 	       //전환 클릭
+	       x = 9;
+    	   for(int i = 0;i<=16;i++) {
+    		   $(".image-attribution").waitUntil(text("테마 이미지 제공:"), 3000);
+ 	 	       $(".sub" + x).click();
+ 	 	       System.out.println("change i is : " + i + " / x is : " + x );
+ 	 	       x++;
+ 	 	   }System.out.println(" -- change click end -- ");
+ 	       //파일 다운로드
+ 	 	   if(TestBrowser.equals("chrome")){
+ 	 		   $(By.linkText("파일 다운로드")).click();
+ 	 		   $(By.linkText("자세한 내용 보기")).click();
+	 	 	   for(int i = 1;i<=3;i++) {
+	 	 		   $(".jpg" + i).click();
+	 	 	   }
+	 	 	   for(int i = 1;i<=3;i++) {
+		 		   $(".gif" + i).click();
+		 	   }
+	 	 	   for(int i = 1;i<=3;i++) {
+		 		   $(".png" + i).click();
+		 	   }
+ 	 	   } else {
+ 	 	 	   //ie는 HTML5 download tag 미지원
+ 	 		   //firefox는 지원한다고 작성되어 있는데 파일 다운이 안됨
+ 	 		   //edge는 다은이름으로 저장 때문에 다운 불가(그래도 다운수로 체크는 되려나?)
+ 	 	   }
+ 	   }
+       
+       @Test(priority = 1)
+       public void egloosTest() {
+ 	       open(egloosUrl);
+ 	       $(By.linkText("TestData")).click();
+ 	       $(By.linkText("외부링크 확인용")).click();
+ 	       $(".banner1").click();
+ 	       open(egloosUrl + "6281874");
+ 	       //마케팅 클릭
+ 	       for(int  i=1;i<=5;i++) {
+ 	    	   $(".sub" + i).click();
+ 	    	   open(egloosUrl + "6281874");
+ 	    	   System.out.println("i is " + i);
+ 	       } System.out.println(" -- marketing click end -- ");
+ 	       //인하우스 클릭
+ 	       for (int i=6;i<=8;i++) {
+ 	    	   $(".sub" + i).click();
+ 	    	   open(egloosUrl + "6281874");
+ 	    	   System.out.println("i is " + i);
+ 	       } System.out.println(" -- inhouse click end -- ");
+ 	       //전환 클릭
+ 	       for (int i=9;i<=25;i++) {
+ 	    	   $(".sub" + i).click();
+   	    	   open(egloosUrl + "6281874");
+ 	    	   System.out.println("i is " + i);
+ 	       } System.out.println(" -- change click end -- ");
+ 	       if(TestBrowser.equals("chrome")) {
+ 	 	       $(By.linkText("TestData")).click();
+ 	 	       $(By.linkText("file download test")).click();
+ 	 	       for(int i=1;i<=7;i++) {
+ 	 	 	       $(".download" + i).click();
+ 	 	 	       System.out.println("i is " + i);
+ 	 	       } System.out.println(" -- fileDownload click end -- ");
+ 	       } else {
+ 	    	   //ie는 HTML5 download tag 미지원
+ 	 		   //firefox는 지원한다고 작성되어 있는데 파일 다운이 안됨
+ 	 		   //edge는 다은이름으로 저장 때문에 다운 불가(그래도 다운수로 체크는 되려나?)
+ 	       }
+       }
+        
+       @AfterClass
+       public void afterTest() {
+    	  closeWebDriver();
+       }       
+}   
